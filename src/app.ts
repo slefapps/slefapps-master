a55cb1291c93a8238ab068f17365c28d11454318
kaleemmolani <kaleemmolani370@gmail.com>
2022-07-13 04:58:51 +0530

    add post method in test entity

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {applyRoutes} from "./middlewares/middleware";
 import routes from './routes/index';
 const app = express();
+app.use(express.json());
 applyRoutes(app, routes);
 
 export default app;
--- src/controllers/test.controller.ts
+++ src/controllers/test.controller.ts
@@ -6,3 +6,7 @@ export const  getTest = (req:Request, res:Response) => {
 export const getTestById = (req:Request, res:Response) => {
   res.send(`Hello World! test ${req.params.id}`);
 }
+
+export const createTest = (req:Request, res:Response) => {
+  res.send(`createTest ${req.body.name}`);
+}
